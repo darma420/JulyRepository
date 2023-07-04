45c6697510c88c54cb5bf804eee524220496a6a6
darma420 <dharma.venkatrao378@gmail.com>
2023-07-04 16:29:25 +0530

    general project pushed to github on july 4th

---
+++ MvnSample/src/main/java/com/intense/OpenUrl.java
@@ -0,0 +1,24 @@
+package com.intense;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class OpenUrl {
+	public static WebDriver driver;
+	
+	@Test
+	public void googlePage() throws InterruptedException {
+		
+		WebDriverManager.chromedriver().setup();
+		driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.co.in/");
+		Thread.sleep(5000);
+		driver.close();
+		
+	}
+
+}
