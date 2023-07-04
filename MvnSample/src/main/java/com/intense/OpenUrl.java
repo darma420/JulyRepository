3b77d56a93e804e7a92ad811e97ebc509c1227f3
darma420 <dharma.venkatrao378@gmail.com>
2023-07-04 17:49:57 +0530

    updated twentytwo line

--- MvnSample/src/main/java/com/intense/OpenUrl.java
+++ MvnSample/src/main/java/com/intense/OpenUrl.java
@@ -1,5 +1,6 @@
 package com.intense;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
@@ -18,7 +19,8 @@ public class OpenUrl {
 		driver.get("https://www.google.co.in/");
 		Thread.sleep(5000);
 		driver.close();
-                driver.quit();
+        driver.quit();
+        driver.findElement(By.name("q")).sendKeys("dadadadad");
 		
 	}
 
